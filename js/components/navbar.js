5cb9ce64761c6f6bccf1a7269ff9337d5af61c5c
Julianv19 <messiocampo8@gmail.com>
2026-04-27 12:53:14 -0500

    feat:actualizacion

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded',function(){
-const navbarElement = document.querySelector('.navbar-container')
-if(!navbarElement) return;
+
+    const navbarElement = document.querySelector('.navbar-container')
+
+    if(!navbarElement) return;
+
     fetch("/views/components/navbar.html")
 // La respuesta la convierte a texto plano
     .then(response => response.text())
 // Inserta el contenido HTML del navbar en el contendor correspondiente
-    .then(data =>{
-        navbarElement.innerHTML= data;
+    .then(data => {
+        navbarElement.innerHTML = data;
     
 
         const currentPage = window.location.pathname.split("/").pop()|| 
@@ -14,7 +17,7 @@ if(!navbarElement) return;
         const navLinks = navbarElement.querySelectorAll('.navbar__link')
         navLinks.forEach(link=>{
             // Verifica si el href del enlace incluye el nombre de la pagina actual
-            if (link.getAttribute('href').includes(currentPage)){
+            if (link.getAttribute('href')?.includes(currentPage)){
                 link.classList.add('active')
             }
         })
